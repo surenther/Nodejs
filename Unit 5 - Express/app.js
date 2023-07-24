553a551e1494fe4bcbde0b30951a916804b53284
Surenther Selvaraj <surenther.ss@gmail.com>
2023-07-24 15:50:35 -0400

    express routes

--- Unit 5 - Express/app.js
+++ Unit 5 - Express/app.js
@@ -1,18 +1,15 @@
-const http = require ('http');
 const express = require ('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log ('In the middleware');
-    next();
+app.use('/add-product',(req,res,next) => {
+    console.log ('In the Another middleware');
+    res.send('<h1>The add Productpage</h1>');
 });
 
-app.use((req,res,next) => {
+app.use('/',(req,res,next) => {
     console.log ('In the Another middleware');
     res.send('<h1>Hello from Express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen (3000)
