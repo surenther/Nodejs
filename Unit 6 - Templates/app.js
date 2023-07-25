e05d6a28b1e0f8b70bb296abc76185ccc9441513
Surenther Selvaraj <surenther.ss@gmail.com>
2023-07-25 13:30:54 -0400

    Add pug template

--- Unit 6 - Templates/app.js
+++ Unit 6 - Templates/app.js
@@ -1,12 +1,13 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const app = express();
+app.set('view engine','pug');
+app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
