4bf6f00a8a084a9992afe6378fc8cb63a3dc831f
Surenther Selvaraj <surenther.ss@gmail.com>
2023-07-26 23:06:19 -0400

    404 controllers

--- Unit 7 - MVC/app.js
+++ Unit 7 - MVC/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorControllers = require('./controllers/error');
 
 const app = express()
 
@@ -16,8 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
-});
+app.use(errorControllers.get404);
 
 app.listen(3000);
