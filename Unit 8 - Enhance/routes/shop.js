32d0693283406acf35ccf68b21eaeccd2e78a442
Surenther Selvaraj <surenther.ss@gmail.com>
2023-07-27 16:19:43 -0400

    Editing and Deleting Products

--- Unit 8 - Enhance/routes/shop.js
+++ Unit 8 - Enhance/routes/shop.js
@@ -10,5 +10,6 @@ router.get('/', ShopController.getIndex);
 router.get('/products', ShopController.getProducts);
 router.get('/cart', ShopController.getCart);
 router.get('/checkout',ShopController.getCheckout);
+router.get('/orders',ShopController.getOrders);
 
 module.exports = router;
